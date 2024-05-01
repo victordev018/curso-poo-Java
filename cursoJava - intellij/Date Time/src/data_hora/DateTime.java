3a1ea7cf4860617b7d150e2cd471de52f2a2ce16
João VIctor <133924640+victordev018@users.noreply.github.com>
2024-05-01 01:32:36 -0300

    Instanciando Objetos dos tipos data-hora

--- cursoJava - intellij/Date Time/src/data_hora/DateTime.java
+++ cursoJava - intellij/Date Time/src/data_hora/DateTime.java
@@ -1,4 +1,27 @@
 package data_hora;
 
+import java.io.*;
+//importando Classes de DateTime (local e Global)
+import java.time.Instant;  // -> Global(com fuso horário)
+import java.time.LocalDate;     //-> Local (data)
+import java.time.LocalDateTime;  // -> Local (Date e Time)
+
+
 public class DateTime {
+
+    public static void main(String[] args){
+        //Instanciando Datas e Horários Atua
+        LocalDate d01 = LocalDate.now();  // data Local
+        LocalDateTime d02 = LocalDateTime.now(); // data e hora local
+        Instant d03 = Instant.now(); // data e hora Global(padrão GMT/UTC) -> Londres
+
+
+        // exbibindo Datas e Horários:
+        System.out.println("D01: " + d01);
+        System.out.println("D02: " + d02);
+        System.out.println("D03: " + d03);  // formato ISO 8601
+
+
+    }
+
 }

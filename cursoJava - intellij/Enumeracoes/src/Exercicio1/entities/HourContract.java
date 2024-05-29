87c50262ed3086aa7d9d03ab1c0c6cbb4f9260fc
João VIctor <joawrt8520@gmail.com>
2024-05-29 20:44:24 -0300

    feat: added functionality that returns the total value of that contract

--- cursoJava - intellij/Enumeracoes/src/Exercicio1/entities/HourContract.java
+++ cursoJava - intellij/Enumeracoes/src/Exercicio1/entities/HourContract.java
@@ -41,4 +41,9 @@ public class HourContract {
         this.hours = hours;
     }
 
+    // método que retorna valor total desse contrato.
+    public double totalValue(){
+        return hours * valuePerHour;
+    }
+
 }

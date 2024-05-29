f5e0730dfc9d172ed720bf853ad6b8af3e947361
João VIctor <joawrt8520@gmail.com>
2024-05-29 19:55:51 -0300

    feat: Added constructors and get and set of class Departament

--- cursoJava - intellij/Enumeracoes/src/Exercicio1/entities/Departament.java
+++ cursoJava - intellij/Enumeracoes/src/Exercicio1/entities/Departament.java
@@ -3,10 +3,20 @@ package Exercicio1.entities;
 public class Departament {
     private String name;
 
+    // construtores
     public Departament(){
     }
 
     public Departament(String name){
+        this.name = name;
+    }
+
+    // getrs e setrs
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
         this.name = name;
     }
 }

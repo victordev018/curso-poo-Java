4de0f111c476bc6279b1bf685d16e458acb99a30
João VIctor <joawrt8520@gmail.com>
2024-05-29 19:51:21 -0300

    feat: Criated class Departament

---
+++ cursoJava - intellij/Enumeracoes/src/Exercicio1/entities/Departament.java
@@ -0,0 +1,12 @@
+package Exercicio1.entities;
+
+public class Departament {
+    private String name;
+
+    public Departament(){
+    }
+
+    public Departament(String name){
+        this.name = name;
+    }
+}

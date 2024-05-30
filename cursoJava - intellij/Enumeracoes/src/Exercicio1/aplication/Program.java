c3daf203301cf4ff2f61498a363cb2d84169befe
João VIctor <joawrt8520@gmail.com>
2024-05-30 13:43:57 -0300

    feat: created worker data entry

--- cursoJava - intellij/Enumeracoes/src/Exercicio1/aplication/Program.java
+++ cursoJava - intellij/Enumeracoes/src/Exercicio1/aplication/Program.java
@@ -9,6 +9,16 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner in = new Scanner(System.in);
 
+        // entrada de dados do trabalhador:
+        System.out.print("Enter departament's name: ");
+        String departamentName = in.nextLine();
+        System.out.print("Enter worker data:");
+        System.out.print("Name: ");
+        String workerName = in.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = in.nextLine();
+        System.out.print("Base salary: ");
+        Double baseSalary = in.nextDouble();
 
 
         in.close();

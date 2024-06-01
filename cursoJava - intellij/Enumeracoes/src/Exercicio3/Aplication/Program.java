4c0c8af9d2b69e55cba67e01cf6a0fb8b933ab07
João VIctor <joawrt8520@gmail.com>
2024-06-01 00:06:16 -0300

    feat: Created class Product

---
+++ cursoJava - intellij/Enumeracoes/src/Exercicio3/Aplication/Program.java
@@ -0,0 +1,16 @@
+package Exercicio3.Aplication;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Program {
+
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner in = new Scanner(System.in);
+
+
+        in.close();
+    }
+
+}

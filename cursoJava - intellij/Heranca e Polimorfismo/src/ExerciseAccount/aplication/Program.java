5576c9bfb5180cec2293447ead7daa5f1a55fd80
João VIctor <joawrt8520@gmail.com>
2024-06-01 20:20:11 -0300

    feat: Created class Account in exercise3

---
+++ cursoJava - intellij/Heranca e Polimorfismo/src/ExerciseAccount/aplication/Program.java
@@ -0,0 +1,14 @@
+package ExerciseAccount.aplication;
+
+
+import ExerciseAccount.entities.Account;
+
+public class Program {
+
+    public static void main(String[] args){
+
+        
+
+    }
+
+}
---
+++ cursoJava - intellij/Heranca e Polimorfismo/src/ExerciseAccount/entities/Account.java
@@ -0,0 +1,48 @@
+package ExerciseAccount.entities;
+
+public class Account {
+
+    // atributos
+    private Integer number;
+    private String holder;
+    private Double balance;
+
+    // construtores
+    public Account(){
+    }
+
+    public Account (Integer number, String holder, Double balance){
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    // get e set
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    // métodos
+
+    // método para fazer um saque
+    public void withDraw(Double amount){
+        this.balance -= amount;
+    }
+
+    // método para realizar um depósito
+    public void deposit(Double amount){
+        this.balance += amount;
+    }
+}

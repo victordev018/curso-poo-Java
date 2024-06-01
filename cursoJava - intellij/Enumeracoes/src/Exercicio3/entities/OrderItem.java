e6b7686f83055eecaec75e9301eafb338a094278
João VIctor <joawrt8520@gmail.com>
2024-06-01 00:17:53 -0300

    feat: Created class OrderItem

---
+++ cursoJava - intellij/Enumeracoes/src/Exercicio3/entities/OrderItem.java
@@ -0,0 +1,37 @@
+package Exercicio3.entities;
+
+public class OrderItem {
+
+    // atributos
+    private Integer quantity;
+    private Double price;
+
+    private Product product;
+
+    // construtores
+    public OrderItem(){
+    }
+
+    public OrderItem(int quantity, double price, Product product){
+        this.product = product;
+        this.quantity = quantity;
+        this.price = product.getPrice();
+    }
+
+    // geters
+    public Integer getQuantity(){
+        return quantity;
+    }
+    public Double getPrice(){
+        return price;
+    }
+    public Product getProduct(){
+        return product;
+    }
+
+    // método que retorna o valor total desse OrderItem
+    public Double subTotal(){
+        return quantity * price;
+    }
+
+}

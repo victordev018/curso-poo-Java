f2936bf5157d7bd488ff5e1a7cc414c57aded79e
João VIctor <joawrt8520@gmail.com>
2024-06-17 15:35:20 -0300

    feat: Created class of the exception

---
+++ cursoJava - intellij/Treatment of Exception/src/ReservationExercise/exceptions/DomainException.java
@@ -0,0 +1,12 @@
+package ReservationExercise.exceptions;
+
+public class DomainException extends RuntimeException {
+    // definindo a versão do serialVersinUID
+    private static final long serialVersionUID = 1L;
+
+    // construtor
+    public DomainException (String message){
+        super(message);
+    }
+
+}

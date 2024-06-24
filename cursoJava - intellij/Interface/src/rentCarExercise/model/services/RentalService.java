8bbc48a25d096266301577d56501826af0ea3dcf
João VIctor <joawrt8520@gmail.com>
2024-06-23 21:42:15 -0300

    feat: logic implementation of the processInvoice method

--- cursoJava - intellij/Interface/src/rentCarExercise/model/services/RentalService.java
+++ cursoJava - intellij/Interface/src/rentCarExercise/model/services/RentalService.java
@@ -3,6 +3,8 @@ package rentCarExercise.model.services;
 import rentCarExercise.model.entities.CarRental;
 import rentCarExercise.model.entities.Invoice;
 
+import java.time.Duration;
+
 // classe de serviço de aluguel
 public class RentalService {
 
@@ -22,6 +24,27 @@ public class RentalService {
 
     // método para processar a fatura de um determinado aluguel de carro
     public void processInvoice(CarRental carRental){
-        carRental.setInvoice(new Invoice(50.00, 10.00));
+
+        // pegando a duração entre start e finish em minutos
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        // pconvertendo os minutos para hora em forma de fração (ajuda para fazer o arredondamento)
+        double hours = minutes / 60;
+
+        // variável para guardar o pagamento básico
+        double basicPayment;
+
+        // se a quantidade de horas for menor igual a 12, será calculada com o preço por hora
+        if (hours <= 12.0){
+            basicPayment = pricePerHour * Math.ceil(hours);     // Math.ceil arredonda o valor para cima
+        }
+        // se não, será calculado com o preço por dia
+        else{
+            basicPayment = pricePerDay * Math.ceil(hours / 24); // trata a duração em dias
+        }
+
+        // calculando o imposto
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
     }
 }

adecd578ed21537de88b869ce90e70a3b12f645a
João VIctor <joawrt8520@gmail.com>
2024-06-24 15:07:42 -0300

    feat: implementation of the TaxService interface

---
+++ cursoJava - intellij/Interface/src/rentCarExercise/model/services/TaxService.java
@@ -0,0 +1,9 @@
+package rentCarExercise.model.services;
+
+// a classe que implementar esta interface, deve implememtar seus métodos aqui declarados
+public interface TaxService {
+
+    // declaração do método tax(), que recebe um amount e deve retornar um valor de imposto
+    public double tax(double amount);
+
+}

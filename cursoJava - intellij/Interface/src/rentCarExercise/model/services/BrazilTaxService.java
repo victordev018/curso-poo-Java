e5b5a49243ec83689a8be654b926fb7c1f556650
João VIctor <joawrt8520@gmail.com>
2024-06-23 21:09:28 -0300

    feat: created the BrazilTaxService service class

---
+++ cursoJava - intellij/Interface/src/rentCarExercise/model/services/BrazilTaxService.java
@@ -0,0 +1,16 @@
+package rentCarExercise.model.services;
+
+// serviço sobre o imposto no brazil
+public class BrazilTaxService {
+
+    // método que retorna a quantia de imposto baseado em uma quantidade recebida
+    public double tax(double amount){
+        // se a quantia for menor ou igual a 100, a taxa é 20%
+        if (amount <= 100.00){
+            return amount * 0.2;
+        }
+        // se não for, a taxa é de 15%
+        return amount * 0.15;
+    }
+
+}

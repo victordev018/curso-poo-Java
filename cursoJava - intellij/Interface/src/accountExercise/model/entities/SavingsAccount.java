daaf8801b5e0d4b5d0c624b493d1893bcd870307
João VIctor <joawrt8520@gmail.com>
2024-07-01 14:20:18 -0300

    feat: logic implementation in methods of the SavingsAccount class

--- cursoJava - intellij/Interface/src/accountExercise/model/entities/SavingsAccount.java
+++ cursoJava - intellij/Interface/src/accountExercise/model/entities/SavingsAccount.java
@@ -1,5 +1,7 @@
 package accountExercise.model.entities;
 
+import accountExercise.model.exceptions.TransationException;
+
 // classe que representa a conta do tupo poupança
 public class SavingsAccount implements Account{
 
@@ -42,23 +44,41 @@ public class SavingsAccount implements Account{
     // sobreposição do método que realiza um depósito
     @Override
     public void depossit(Double amount) {
-
+        // adiciona valor ao saldo da conta
+        balance += amount;
     }
     // sobreposição do método que realiza saque
     @Override
     public void withDraw(Double amount) {
-
+        // se o saldo for o suficiente
+        if (amount <= balance){
+            // retira o valor do saldo
+            balance -= amount;
+        }
+        else{
+            throw new TransationException("Insufficient funds");
+        }
     }
 
     // sobreposição do método que realiza uma transferência
     @Override
     public void transfer(Account targetAccount, Double amount) {
-
+        // verifica se o valor a ser transferido esta de acordo com o saldo da conta
+        if (amount <= balance){
+            // retira o valor da conta de origem
+            balance -= amount;
+            // deposita valor na conta destino
+            targetAccount.depossit(amount);
+        }
+        else{
+            throw new TransationException("insufficient funds");
+        }
     }
 
     // sobreposição do método que checa o saldo da conta
     @Override
     public double checkBalance() {
-        return 0;
+        // retorna o valor do saldo
+        return getBalance();
     }
 }

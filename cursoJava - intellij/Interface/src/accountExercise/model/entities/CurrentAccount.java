558f2916b12dce80f0516d22ab9a79ca816c07d6
João VIctor <joawrt8520@gmail.com>
2024-07-01 14:13:39 -0300

    feat: logic implementation in methods of the CurrentAccount class

--- cursoJava - intellij/Interface/src/accountExercise/model/entities/CurrentAccount.java
+++ cursoJava - intellij/Interface/src/accountExercise/model/entities/CurrentAccount.java
@@ -1,5 +1,7 @@
 package accountExercise.model.entities;
 
+import accountExercise.model.exceptions.TransationException;
+
 // classe que representa a conta do tipo corrente
 public class CurrentAccount implements Account{
 
@@ -43,23 +45,41 @@ public class CurrentAccount implements Account{
     // sobreposição do método que realiza um depósito
     @Override
     public void depossit(Double amount) {
-
+        // acrescenta valor no saldo
+        this.balance += amount;
     }
     // sobreposição do método que realiza saque
     @Override
     public void withDraw(Double amount) {
-
+        // se o valor for menor ou igual ao saldo e se estiver dentro do limite
+        if (amount <= balance && amount <= limit){
+            // retira o valor do saldo
+            balance -= amount;
+        }
+        else{
+            throw new TransationException("insufficient funds");
+        }
     }
 
     // sobreposição do método que realiza uma transferência
     @Override
     public void transfer(Account targetAccount, Double amount) {
-
+        // verifica se o valor a ser transferido esta de acordo com o saldo da conta
+        if (amount <= balance){
+            // retira o valor da conta de origem
+            balance -= amount;
+            // deposita valor na conta destino
+            targetAccount.depossit(amount);
+        }
+        else{
+            throw new TransationException("insufficient funds");
+        }
     }
 
     // sobreposição do método que checa o saldo da conta
     @Override
     public double checkBalance() {
-        return 0;
+        // retorna o valor do saldo
+        return getBalance();
     }
 }

71a7e0c0b14d294f1b29f6b97850091a96ef3f86
João VIctor <joawrt8520@gmail.com>
2024-07-01 15:57:42 -0300

    feat: Implementation PrintService service in generics example

---
+++ cursoJava - intellij/GENERICS-SET-MAP/src/Generics/exampleGenerics/services/PrintService.java
@@ -0,0 +1,38 @@
+package Generics.exampleGenerics.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// serviço printService -> parametrizada por um tipo Type
+public class PrintService<Type> {
+
+    // lista de objetos do tipo Type
+    List<Type> list = new ArrayList<>();
+
+    // método para adicionar um novo elemento na lista
+    public void addValue(Type value){
+        list.add(value);
+    }
+
+    // método para pegar o primeiro valor da lista
+    public Type firs(){
+        // se a lista estiver vazia
+        if (list.isEmpty()){
+            throw new IllegalStateException("Value not fund");
+        }
+        return list.getFirst();
+    }
+
+    // método para printar a lista
+    public void print(){
+        System.out.println("[");
+        if (!list.isEmpty()){
+            System.out.print(list.get(0));
+        }
+        for (int i = 1; i < list.size(); i++){
+            System.out.print(", " + list.get(i));
+        }
+        System.out.println("]");
+    }
+
+}

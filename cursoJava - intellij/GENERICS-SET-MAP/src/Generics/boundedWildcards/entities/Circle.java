24aad97f201e607b3c0e3c49891d6decbe7da9a8
João VIctor <joawrt8520@gmail.com>
2024-07-04 10:51:11 -0300

    feat: basic implementation of the Circle class

---
+++ cursoJava - intellij/GENERICS-SET-MAP/src/Generics/boundedWildcards/entities/Circle.java
@@ -0,0 +1,20 @@
+package Generics.boundedWildcards.entities;
+
+public class Circle implements Shape{
+
+    private static final double PI = Math.PI;
+
+    // atributos
+    private double radius;  // raio
+
+    // get e set
+    public double getRadius(){return radius;}
+
+    public void setRadius(Double radius){this.radius = radius;}
+
+    // sobreposição do método area da interface Shape
+    @Override
+    public double area(){
+        return PI * radius * radius;
+    }
+}

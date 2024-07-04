9984aaf389f50f44c1196355a1cca00a3b0c9e39
João VIctor <joawrt8520@gmail.com>
2024-07-04 11:09:55 -0300

    feat: method totalArea more generic for Shapes

--- cursoJava - intellij/GENERICS-SET-MAP/src/Generics/boundedWildcards/application/Program.java
+++ cursoJava - intellij/GENERICS-SET-MAP/src/Generics/boundedWildcards/application/Program.java
@@ -12,16 +12,23 @@ public class Program {
         // ciando lista de Shapes
         List<Shape> listShapes = new ArrayList<>();
 
+        // ciando lista de Círculos
+        List<Circle> listCircles = new ArrayList<>();
+
         // adicionando Shapes na lista
         listShapes.add(new Circle(4.5));
         listShapes.add(new Rectangle(3, 4));
 
-        // exibindo valor total de aresa das formas presentes na lista de Shapes
-        System.out.println("Total area: " + totalArea(listShapes));
+        // adicionando circulos na lista de circulos
+        listCircles.add(new Circle(2.4));
+        listCircles.add(new Circle(5));
+
+        // exibindo valor total de aresa das formas presentes na lista de circulos
+        System.out.println("Total area: " + totalArea(listCircles));
     }
 
-    // método que calcula o total  area de uma lista de Shapes
-    public static Double totalArea(List<Shape> list){
+    // método que calcula o total  area de uma lista de Shapes ou de uma classe que implemente Shape
+    public static Double totalArea(List<? extends Shape> list){
         double sum = 0.0;
         for (Shape s : list){
             sum += s.area();

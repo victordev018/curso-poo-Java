aab3d592e7184096d6b08e70029e1874b3f53005
João VIctor <joawrt8520@gmail.com>
2024-07-08 00:27:49 -0300

    feat: doing tests with the comparison of objects

---
+++ cursoJava - intellij/GENERICS-SET-MAP/src/HashCodeAndEquals/application/Program.java
@@ -0,0 +1,17 @@
+package HashCodeAndEquals.application;
+
+import HashCodeAndEquals.entities.Client;
+
+public class Program {
+    public static void main(String[] args){
+
+        // instanciando dois objetos clienetes e comparando eles
+        Client c1 = new Client("João", "joao@gmail.com");
+        Client c2 = new Client("Maria", "maria@gmail.com");
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+
+    }
+}

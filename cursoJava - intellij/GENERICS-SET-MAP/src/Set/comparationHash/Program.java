f811ac9d7876d97aa3de51e70f6ae4b76a942c0a
João VIctor <joawrt8520@gmail.com>
2024-07-18 16:38:35 -0300

    feat: testing how HashSet tests equality

---
+++ cursoJava - intellij/GENERICS-SET-MAP/src/Set/comparationHash/Program.java
@@ -0,0 +1,24 @@
+package Set.comparationHash;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        // criando um hashSet de Products
+        Set<Product> productSet = new HashSet<>();
+
+        // adiconando produtos na lista
+        productSet.add(new Product("TV", 900.0));
+        productSet.add(new Product("Notebook", 1200.0));
+        productSet.add(new Product("Tablet", 400.0));
+
+        Product prod = new Product("Notebook", 1200.0);
+
+        // verificando se na lista productSet possue o objeto prod
+        System.out.println(productSet.contains(prod));
+
+    }
+}

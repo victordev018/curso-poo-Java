e240d1c196d59b4435cfbec3c52017477cb9521a
João VIctor <joawrt8520@gmail.com>
2024-07-20 14:23:26 -0300

    feat: function exercise lambda : lambda expression declared

--- cursoJava - intellij/Functional and lambda programming/src/functionLambda/exercise/application/Program.java
+++ cursoJava - intellij/Functional and lambda programming/src/functionLambda/exercise/application/Program.java
@@ -5,6 +5,7 @@ import functionLambda.exercise.utils.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
@@ -19,10 +20,13 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
+        // declarando uma expressão lamda de Function
+        Function<Product, String> func = p -> p.getName().toUpperCase();
+
         // criando uma nova lista com os nomes dos produtos de list, em caixa alta
         // a função map(Function<T, R>), aplica uma função para cada elemento de uma stream
 
-        List<String> names =  list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+        List<String> names =  list.stream().map(func).collect(Collectors.toList());
 
         // exibindo a lista de nomes de produtos
         names.forEach(System.out::println);
